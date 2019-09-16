e50171c5f21117517393e4d703916d79e8e678a7
oiojin831 <oiojin831@gmail.com>
2019-09-16 21:24:34 +0900

    count blanket

--- src/Components/Dashboard/Statistic/index.js
+++ src/Components/Dashboard/Statistic/index.js
@@ -4,6 +4,8 @@ import { db } from "../../../firebase.js";
 import { DatePicker, SubmitButton } from "@jbuschke/formik-antd";
 import { formattedNow, getDaysArray, fromISOtoString } from "../../../util";
 import Chart from "react-apexcharts";
+import { jhonorData } from "../../../Constants/roomName";
+import _ from "lodash";
 
 const krwToString = price => {
   if (typeof price === "number") {
@@ -18,6 +20,8 @@ export default () => {
   const [roomFilledFromToday, setRoomFilledFromToday] = useState(0);
   const [totalRoom, setTotalRoom] = useState(0);
   const [totalRoomFromToday, setTotalRoomFromToday] = useState(0);
+  const [blacnketSumDisplay, setBlanketSumDisplay] = useState({});
+  const [blacnketTotalSumDisplay, setBlanketTotalSumDisplay] = useState({});
   const [labels, setLabels] = useState(["reserved", "empty"]);
   const [options, setOptions] = useState({});
   const [series, setSeries] = useState([]);
@@ -40,6 +44,8 @@ export default () => {
       const promises = [];
       let totalFilledSum = 0;
       let totalFilledSumFromToday = 0;
+      let blanketSum = {};
+      let blanketTotalSum = [];
       dates.forEach(date => {
         const p = db
           .collection("reservations")
@@ -52,6 +58,7 @@ export default () => {
         let daySum = 0;
         let dayFilledSum = 0;
         let dayFilledSumFromToday = 0;
+        let oneDayBeds = [];
         snapshot.forEach(doc => {
           let roomSum = 0;
           if (
@@ -80,18 +87,55 @@ export default () => {
           dayFilledSum = dayFilledSum + roomFilledSum;
           dayFilledSumFromToday =
             dayFilledSumFromToday + roomFilledSumFromToday;
+          if (
+            doc.data().checkOutDate === dates[i] &&
+            doc.data().guestHouseName === "jhonor"
+          ) {
+            console.log(
+              doc.data().roomNumber,
+              doc.data().reservationCode,
+              jhonorData[doc.data().roomNumber]
+            );
+            oneDayBeds.push(jhonorData[doc.data().roomNumber].beds);
+          }
         });
+        console.log("onedaybes", oneDayBeds);
+        let singleBlankets = _.sumBy(oneDayBeds, room => {
+          let s = room.single ? room.single : 0;
+          let b = room.bunk ? room.bunk : 0;
+          return s + b * 2;
+        });
+        let queenBlankets = _.sumBy(oneDayBeds, room => {
+          let q = room.queen ? room.queen : 0;
+          return room.queen;
+        });
+        blanketSum[dates[i]] = { single: singleBlankets, queen: queenBlankets };
+        blanketTotalSum.push({ single: singleBlankets, queen: queenBlankets });
         totalSum = daySum + totalSum;
         totalFilledSum = dayFilledSum + totalFilledSum;
         totalFilledSumFromToday =
           dayFilledSumFromToday + totalFilledSumFromToday;
       });
 
+      let singleTotalBlankets = _.sumBy(blanketTotalSum, room => {
+        let s = room.single ? room.single : 0;
+        let b = room.bunk ? room.bunk : 0;
+        return s + b * 2;
+      });
+      let queenTotalBlankets = _.sumBy(blanketTotalSum, room => {
+        let q = room.queen ? room.queen : 0;
+        return room.queen;
+      });
       //   setSeries([num, 30 * 17 - num]);
       //console.log(totalPrice);
       setNum(totalSum);
       setRoomFilled(totalFilledSum);
       setRoomFilledFromToday(totalFilledSumFromToday);
+      setBlanketSumDisplay(blanketSum);
+      setBlanketTotalSumDisplay({
+        single: singleTotalBlankets,
+        queen: queenTotalBlankets
+      });
       actions.setSubmitting(false);
     } catch (error) {
       console.log("error", error.toString());
@@ -130,6 +174,8 @@ export default () => {
               Empty Room from today over whole range:{" "}
               {((totalRoomFromToday - roomFilledFromToday) / totalRoom) * 100} %
             </div>
+            <div>날짜별: {JSON.stringify(blacnketSumDisplay)}</div>
+            <div>총 개수: {JSON.stringify(blacnketTotalSumDisplay)}</div>
 
             {/* <Chart options={options} series={series} type="donut" width="380" />*/}
           </React.Fragment>
